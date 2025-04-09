e90b8de220a81143ed32bf5c674e187953f9978b
Kaushal Damania <damaniakaushal4@gmail.com>
2025-04-08 22:25:43 -0400

    running

--- scripts/Track1/train_1s_binary.sh
+++ scripts/Track1/train_1s_binary.sh
@@ -2,17 +2,18 @@
 set -e
 
 # Default Training Parameters
-data_rootpath="E:/MDPP_data/MPDD-Elderly" # Dataset root directory
+data_rootpath="/Users/kaushaldamania/deepl/MPDD-Elderly" # Dataset root directory
+
 AUDIOFEATURE_METHOD="mfccs" # Audio feature type, options {wav2vec, opensmile, mfccs}
 VIDEOLFEATURE_METHOD="openface" # Video feature type, options {openface, resnet, densenet}
 SPLITWINDOW="1s" # Window duration, options {"1s", "5s"}
 LABELCOUNT=2 # Number of label categories, options {2, 3, 5}
 TRACK_OPTION="Track1"
 FEATURE_MAX_LEN=26 # Set maximum feature length; pad with zeros if insufficient, truncate if exceeding
-BATCH_SIZE=1
+BATCH_SIZE=16
 LR=0.00002
 NUM_EPOCHS=200
-DEVICE="cpu" # Options {cuda, cpu}
+DEVICE="mps" # Options {cuda, cpu}
 
 
 for arg in "$@"; do
